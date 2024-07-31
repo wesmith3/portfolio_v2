d3da243e2da13cbb99dc6b144c20345542c68bad
Wesley <wesmith314@gmail.com>
2024-07-30 23:25:46 -0700

    removes global styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+"use client"
 import styles from "./page.module.css";
+import Stars from './stars.jpg'
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>Hello</h1>
+      <Stars />
     </main>
   );
 }
